648c4062155e4538f8f267b3df7e67f0d1998ffd
Daniel García Moreno <daniel@endlessm.com>
2020-07-30 11:05:10 +0200

    license: Add GPLv2 header to all source files
    https://phabricator.endlessm.com/T30588

--- tools/build-local-flatpak.sh
+++ tools/build-local-flatpak.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
-#
-# Convenience script to build a Flatpak bundle with the local contents (the original manifest)
-# builds from git so changes need to be committed.
-#
-# Copyright (c) 2018 Endless Mobile Inc.
-#
+##
+## Copyright © 2020 Endless OS Foundation LLC.
+##
+## This file is part of hack-game-state-service
+## (see https://github.com/endlessm/hack-game-state-service).
+##
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License along
+## with this program; if not, write to the Free Software Foundation, Inc.,
+## 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+##
 
 set -e
 script_path=$0
